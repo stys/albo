646c02d3a9267e37a2cf4d34151b6e67fba18b79
Alexey Stysin <alexey.stysin@gmail.com>
2020-01-18 23:19:11 +0300

    exponential penalty variation

--- albo/acquisition/objective.py
+++ albo/acquisition/objective.py
@@ -136,3 +136,43 @@ class SmoothAugmentedLagrangianMCObjective(AugmentedLagrangianMCObjective):
         y = x + 1
         z = - 0.25 / x
         return m * torch.where(x > -0.5, y, z)
+
+
+class ExponentialAugmentedLagrangianMCObjective(AugmentedLagrangianMCObjective):
+    r"""
+
+    Iusem1999
+    """
+
+    def __init__(
+        self,
+        objective: Callable[[Tensor], Tensor],
+        constraints: List[Callable[[Tensor], Tensor]],
+        r: float = 100.0,
+        mults: Tensor = None
+    ) -> None:
+        super(ExponentialAugmentedLagrangianMCObjective, self).__init__()
+        self.objective = objective
+        self.constraints = constraints
+        self.r = r
+
+        if mults is None:
+            mults = torch.ones((len(constraints), 1), dtype=float)
+        self.register_buffer('mults', mults)
+
+    def forward(self, samples: Tensor) -> Tensor:
+        obj = self.objective(samples)
+        penalty = torch.zeros_like(obj)
+        for i, constraint in enumerate(self.constraints):
+            penalty += self.penalty(constraint(samples), self.mults[i], self.r)
+        return - (obj + penalty)
+
+    def update_mults(self, samples: Tensor) -> None:
+        for i, constraint in enumerate(self.constraints):
+            self.mults[i] = max(1.e-18, self.grad_penalty(constraint(samples), self.mults[i], self.r).mean())
+
+    def penalty(self, t: Tensor, m: Tensor, r: float) -> Tensor:
+        return (m / r) * (torch.exp(r * t) - 1.0)
+
+    def grad_penalty(self, t: Tensor, m: Tensor, r: float) -> Tensor:
+        return m * torch.exp(r * t)
